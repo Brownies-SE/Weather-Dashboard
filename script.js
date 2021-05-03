caf381c33847553b61ad9cb7a07d54f8cff8f09f
Josh Brown <brownjoshalan@gmail.com>
2021-05-03 07:41:06 -0600

    Added variables to the js script for future use

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+/* Create something to search a city and add it to the search history. Add the current weather data attributes to elements dor user to see.
+UvIndex needs to have a color that represents the conditions. Create 5 elements that displays  day forecast with the current data attributes.
+*/
 
+//Creating all variables
+var myKey = "094ff2fa112c80cd9451fbf9613c1dea";
+var userInput = $(".userInput");
+var searchButton = $(".searchButton");
+var cityEl = $(".cityName");
+var dateEl = $(".currentDate");
+var iconEl = $(".Icon");
+var tempEl = $(".temperature");
+var humidEl = $(".humidity");
+var windEl = $(".wind");
+var uvEl = $(".uvIndex");
