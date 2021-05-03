fd8bcfe5e9451c0638105811e3e65b1b59e4369c
Josh Brown <brownjoshalan@gmail.com>
2021-05-03 09:19:18 -0600

    Again created new elements in HTML so they are all dynamic

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ UvIndex needs to have a color that represents the conditions. Create 5 elements 
 
 //Creating all variables
 var myKey = "094ff2fa112c80cd9451fbf9613c1dea";
-var userInput = $(".userInput");
-var searchButton = $(".searchButton");
-var cityEl = $(".cityName");
-var dateEl = $(".currentDate");
-var iconEl = $(".Icon");
-var tempEl = $(".temperature");
-var humidEl = $(".humidity");
-var windEl = $(".wind");
-var uvEl = $(".uvIndex");
+var searchButton = $("#searchButton");
+var clearButton = $("#clearButton");
+var cityDateEl = $("#cityAndDate");
+var tempEl = $("#temperature");
+var humidEl = $("#humidity");
+var windEl = $("#wind");
+var uvEl = $("#uvIndex");
 var searchHistory = $(".searchHistory");
-var futureForecastEl = $(".card-row");
+var futureForecastEl = $("#fiveDayForecast");
+var historyButtonsEl = $(".btn-group");
 
 var today = moment().format("dddd, MMMM Do YYYY");
 console.log(today);
