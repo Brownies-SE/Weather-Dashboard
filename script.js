866d853d8445756a68e074f724697b47d29684c7
Josh Brown <brownjoshalan@gmail.com>
2021-05-03 21:48:25 -0600

    Added full functionality, added api calls to get uvIndex and to other card decks

--- script.js
+++ script.js
@@ -6,17 +6,21 @@ UvIndex needs to have a color that represents the conditions. Create 5 elements 
 var myKey = "094ff2fa112c80cd9451fbf9613c1dea";
 var searchButton = $("#searchButton");
 var clearButton = $("#clearButton");
-var cityDateEl = $("#cityAndDate");
+var cityEl = $("#city");
+var dateEl = $("#date");
 var tempEl = $("#temperature");
 var humidEl = $("#humidity");
 var windEl = $("#wind");
 var uvEl = $("#uvIndex");
-var searchHistory = $(".searchHistory");
-var futureForecastEl = $("#fiveDayForecast");
+var fiveDayEl = $("#fiveDay");
 var historyButtonsEl = $(".btn-group");
+var icon = $("#icon");
+var deck = $(".card-deck");
 
 var today = moment().format("dddd, MMMM Do YYYY");
 //console.log(today);
+
+//localStorage.setItem("History", JSON.stringify(getCity));
 
 var historyList = JSON.parse(localStorage.getItem("historyList") || "[]");
 //console.log(historyList);
@@ -24,7 +28,6 @@ var historyList = JSON.parse(localStorage.getItem("historyList") || "[]");
 // $(document).ready(function () {
 //   var getCity = $("userInput").val();
 //   $("#getCity").text(getCity);
-//
 
 $("#searchButton").click(function () {
   var getCity = $("#UserInput").val();
@@ -44,9 +47,68 @@ $("#searchButton").click(function () {
     })
     .then(function (data) {
       console.log(data);
+      var today = moment(data.dt, "X").format("MM/DD/YYYY");
       //Set the text items in the html
+      var iconurl =
+        "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+      icon.attr("src", iconurl);
+      cityEl.text(data.name);
+      dateEl.text(today);
       tempEl.text(data.main.temp);
       humidEl.text(data.main.humidity);
       windEl.text(data.wind.speed);
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${myKey}&units=imperial`
+      )
+        .then(function (reply) {
+          console.log(reply);
+          return reply.json();
+        })
+        .then(function (uvData) {
+          console.log(uvData);
+
+          uvEl.text(uvData.current.uvi);
+        });
     });
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${getCity}&appid=${myKey}&units=imperial`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //console.log(data);
+
+      for (var i = 0; i < data.list.length; i++) {
+        if (data.list[i].dt_txt.includes("12:00:00")) {
+          console.log(data.list[i]);
+          var icon =
+            "http://openweathermap.org/img/w/" +
+            data.list[i].weather[0].icon +
+            ".png";
+          var date = moment(data.list[i].dt, "X").format("MM/DD/YYYY");
+          var temp = data.list[i].main.temp;
+          var wind = data.list[i].wind.speed;
+          var humid = data.list[i].main.humidity;
+
+          deck.append(`   
+             <div class="card">
+          <div class="card-body">
+            <img src="${icon}" alt="" />
+            <p>Date: ${date}</p>
+            <p>temp: ${temp}</p>
+            <p>wind: ${wind}</p>
+            <p>humidity: ${humid}</p>
+          </div>
+        </div>
+`);
+        }
+      }
+    });
+});
+
+clearButton.on("click", function () {
+  localStorage.clear();
 });
